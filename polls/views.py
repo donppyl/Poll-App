fd98e2f94e0007080632e854f133007ab962d3dd
donppyl <donppyl@users.noreply.github.com>
2020-10-17 17:55:07 +0400

    Update View.py file

---
+++ polls/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+
+def car(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+# Create your views here.
